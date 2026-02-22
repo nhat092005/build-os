d9bc769a13dc43eca8dc32ced2d5584bbf14ac56
nhat092005 <minhnhat092005@gmail.com>
2026-02-22 16:03:21 +0700

    docs: update header comment for GPIO LED control utility

--- drivers/gpio-leds/userspace/tools/gpio-leds-ctl.c
+++ drivers/gpio-leds/userspace/tools/gpio-leds-ctl.c
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: GPL-2.0
 /*
- * gpio-leds-ctl - LED Control Utility
+ * GPIO LED Control Utility
  *
  * This utility allows users to control GPIO LEDs from the command line.
  * It supports turning LEDs on/off, setting brightness, configuring triggers,
